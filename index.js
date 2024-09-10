f48da3436135ce987923fe8aaebc693b2f39a410
Anderson Lima <anderson.ariel3291@gmail.com>
2024-09-10 13:29:45 -0300

    fundamentos 003

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 // arrays, objetos
 let meta = {
     value: "Ler um livro por mês",
+    //address: 2,
     checked: false,
-    log: (info) => {
+    /*log: (info) => {
         console.log(info)
-    }
+    }*/
 }
 
+let metas = [
+    meta,
+    {
+        value: 'Caminhas 20 minutos todos os dias',
+        checked: false
+    }
+]
+/*
 meta.log(meta.value)
 
 //function // arrow function
@@ -14,3 +23,7 @@ const criarMeta = () => {}
 function  criaMeta() {
 
 }
+*/
+
+console.log(meta.value)
+console.log(metas[1].value)
